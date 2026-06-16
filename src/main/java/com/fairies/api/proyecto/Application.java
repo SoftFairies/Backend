c8c10983656e645d8c4515341210befe3c05c42a
minmyn <elpezbanan0muak@gmail.com>
2026-06-15 18:35:16 -0600

    first commit

---
+++ src/main/java/com/fairies/api/proyecto/Application.java
@@ -0,0 +1,21 @@
+package com.fairies.api.proyecto;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class Application {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+		SpringApplication.run(Application.class, args);
+		System.out.println("Corriendo en el puerto 8080");
+	}
+
+}
