75fb7b5bab7177733c22042494b97a5636acc10b
minmyn <elpezbanan0muak@gmail.com>
2026-07-01 22:39:14 -0600

    feat(auth): award initial badge upon user registration

--- src/main/java/com/fairies/api/proyecto/modules/auth/application/RegisterUseCase.java
+++ src/main/java/com/fairies/api/proyecto/modules/auth/application/RegisterUseCase.java
@@ -1,27 +1,22 @@
 package com.fairies.api.proyecto.modules.auth.application;
 
 import com.fairies.api.proyecto.common.application.security.PasswordHasher;
+import com.fairies.api.proyecto.modules.gamification.application.AwardBadgeUseCase;
 import com.fairies.api.proyecto.modules.user.domain.model.Role;
 import com.fairies.api.proyecto.modules.user.domain.model.User;
 import com.fairies.api.proyecto.modules.user.infrastructure.persistence.RoleRepository;
 import com.fairies.api.proyecto.modules.user.infrastructure.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class RegisterUseCase {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordHasher passwordHasher;
-
-    public RegisterUseCase(
-            UserRepository userRepository,
-            RoleRepository roleRepository,
-            PasswordHasher passwordHasher) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordHasher = passwordHasher;
-    }
+    private final AwardBadgeUseCase awardBadgeUseCase;
 
     public User execute(User newUser) {
         if (userRepository.findByEmail(newUser.getEmail()).isPresent()) {
@@ -34,6 +29,10 @@ public class RegisterUseCase {
         newUser.setPassword(passwordHasher.hash(newUser.getPassword()));
         newUser.setRole(role);
 
-        return userRepository.save(newUser);
+        User savedUser = userRepository.save(newUser);
+
+        awardBadgeUseCase.execute(savedUser.getId(), 8L);
+
+        return savedUser;
     }
 }
