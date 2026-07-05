6580ea5cec84f1ccb8f148d45aa8f7ab99afdf90
minmyn <elpezbanan0muak@gmail.com>
2026-07-04 18:24:59 -0600

    feat(user): add attribute to entity, fix update logic and refactor constructors

--- src/main/java/com/fairies/api/proyecto/modules/user/application/GetByIdUserUseCase.java
+++ src/main/java/com/fairies/api/proyecto/modules/user/application/GetByIdUserUseCase.java
@@ -2,18 +2,16 @@ package com.fairies.api.proyecto.modules.user.application;
 
 import com.fairies.api.proyecto.modules.user.domain.model.User;
 import com.fairies.api.proyecto.modules.user.infrastructure.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import java.util.Optional;
 import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class GetByIdUserUseCase {
 
     private final UserRepository userRepository;
-
-    public GetByIdUserUseCase(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public Optional<User> execute(UUID id) {
         return userRepository.findById(id);
