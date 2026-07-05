f28cd21ad7a67f6b4a1a9dc6031dd582c6a8897b
Argelia Gonzalez <niniveargeliaperezgonzalez@gmail.com>
2026-07-05 02:24:24 -0600

    wip: creando el modulo y estructura inicial de radar de curiosidades

---
+++ src/main/java/com/fairies/api/proyecto/modules/Curiosityradar/domain/model/curiosity.java
@@ -0,0 +1,4 @@
+package com.fairies.api.proyecto.modules.Curiosityradar.domain.model;
+
+public class curiosity {
+}
---
+++ src/main/java/com/fairies/api/proyecto/modules/Curiosityradar/infrastructure/persistence/CuriosityRepository.java
@@ -0,0 +1,4 @@
+package com.fairies.api.proyecto.modules.Curiosityradar.infrastructure.persistence;
+
+public class CuriosityRepository {
+}
